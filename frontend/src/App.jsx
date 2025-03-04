8063b9c9d8e6f821fad81003dfff13bb3bb45160
bdebilzan <bdebilzan@mac.com>
2025-03-04 14:30:32 -0800

    Create React app for movie app project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import "./css/App.css";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+
+//https://www.youtube.com/watch?v=G6D9cBaLViA
+//51:09
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
